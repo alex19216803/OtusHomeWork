0ef17280a952ad1e67e11a6f5009a13bbfd12872
Netunaev A. V. <netunaevav@suek.ru>
2026-04-07 21:17:22 +0300

    Исправление замечаний по ДЗ 7

--- OtusHomeWork7Serialize/SampleClass.cs
+++ OtusHomeWork7Serialize/SampleClass.cs
@@ -3,8 +3,12 @@ namespace OtusHomeWork7Serialize;
 
 public class F
 {
-    public int i1, i2, i3, i4, i5;
-
+    public int i1 { get; set; }
+    public int i2 { get; set; }
+    public int i3 { get; set; }
+    public int i4 { get; set; }
+    public int i5 { get; set; }
+    
 
     public static F Get() => new F { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
 
